46826feec6c08ec40550e6b091e7ce3e573d8f8d
AnitaFitzwaterStevens <35937725+AnitaFitzwaterStevens@users.noreply.github.com>
2018-01-30 17:44:05 -0600

    Add files via upload

---
+++ groceryList.java
@@ -0,0 +1,58 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+public class groceryList
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        String ItemOne;
+        String ItemTwo;
+        String ItemThree;
+        int ItemOneQuant;
+        int ItemTwoQuant;
+        int ItemThreeQuant;
+        Float ItemOnePrice;
+        Float ItemTwoPrice;
+        Float ItemThreePrice;
+        Float TotalPrice;
+        {
+            //Code below this line
+
+            out.println("Please list three items on your grocery shopping list.");
+            out.println("Item 1?");
+            ItemOne = keyboard.next();
+            out.println("Item 2?");
+            ItemTwo = keyboard.next();
+            out.println("Item 3?");
+            ItemThree = keyboard.next();
+            out.println("");
+            out.println("Now, please enter the quantity of each item.");
+            out.println("How many " + ItemOne + "?");
+            ItemOneQuant = keyboard.nextInt();            
+            out.println("");
+            out.println("How many " + ItemTwo + "?");
+            ItemTwoQuant = keyboard.nextInt();
+            out.println("");
+            out.println("How many " + ItemThree + "?");
+            ItemThreeQuant = keyboard.nextInt();
+            out.println("");
+            out.println("How much does one " + ItemOne + " cost?");
+            ItemOnePrice = keyboard.nextFloat();
+            out.println("");
+            out.println("How much does one " + ItemTwo + " cost?");
+            ItemTwoPrice = keyboard.nextFloat();
+            out.println("");
+            out.println("How much does one " + ItemThree + " cost?");
+            ItemThreePrice = keyboard.nextFloat();
+            out.println("");
+            out.println("Calculating your grocery bill.");
+            TotalPrice =  ((ItemOneQuant * ItemOnePrice) + (ItemTwoQuant * ItemTwoPrice) + (ItemThreeQuant * ItemThreePrice));
+            out.println("Your total cost is " + TotalPrice);
+
+        
+
+        }
+
+    }
+}
