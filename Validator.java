8ba36005616df8f55ff2b1495939f2c1e5b0c2df
AnitaFitzwaterStevens <35937725+AnitaFitzwaterStevens@users.noreply.github.com>
2018-03-05 15:10:06 -0600

    Add files via upload

--- Validator.java
+++ Validator.java
@@ -1,47 +1,51 @@
 package com.company;
-
-import static java.lang.System.out;
-
 import java.util.Scanner;
 
 public class Validator {
 
-    public static void main(String[] args) {
+    public static int getIntegerBetween(int lower, int upper) {
         Scanner keyboard = new Scanner(System.in);
+        int number;
+
+        do {
+            number = keyboard.nextInt();
+
+            if (number < lower || number > upper)
+            {
+                System.out.println("That number is not in range.  Try again.");
+
+            }
+
+        } while (number < lower || number > upper);
+        return number;
+    }
+
+
+
+    public static int getTensInteger(int lower, int upper) {
+        Scanner keyboard = new Scanner(System.in);
+        int number;
+        do {
+            number = keyboard.nextInt();
+
+            if ((number % 10 != 0) || ((number < lower) || (number > upper)))
+            {
+                System.out.println("Error.  Invalid number entered.  Try again.");
+            }
+        } while ((number % 10 != 0) || (number < lower || number > upper));
+        return number;
+    }
+
+
+    public static void main(String[] args) {
+
         System.out.println("Please enter a number between 1 and 10.");
         int firstNumber = getIntegerBetween(1, 10);
-        do {
-            firstNumber = keyboard.nextInt();
-            boolean keepGoing;
-            keepGoing = true;
-            if ((firstNumber < 1) || (firstNumber > 10)) {
-                System.out.print("Sorry, that number isn't in range. Try again.");
-                keepGoing = false;
-            } else {
-                System.out.println("Please enter a number between 200 and 3000 that is divisible by 10.");
-                int secondNumber = getTensInteger(200, 3000);
-                secondNumber = keyboard.nextInt();
-                keepGoing = true;
-                if ((secondNumber % 10 != 0) && ((secondNumber < 200) || (secondNumber > 3000))) {
-                    System.out.println("Error. That is not a valid number.  Try again.");
-                    keepGoing = true;
-                } else {
-                    System.out.println("You entered " + firstNumber + " and " + secondNumber);
-                }
-            }
-        } while (!keepGoing);
-        System.out.println("Thank you for using the Validator.");
 
+
+        System.out.println("Please enter a number between 200 and 3000 that is a multiple of ten.");
+        int secondNumber = getTensInteger(200, 3000);
+
+        System.out.println("You entered " + firstNumber + " and " + secondNumber);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
